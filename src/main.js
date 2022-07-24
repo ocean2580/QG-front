a8fbe71dece767738087bec421e0ac685166cc8b
Ocean <2080917477@qq.com>
2022-07-24 11:58:54 +0800

    后台框架搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/global.css'
+/**
+ * 用于引入文件
+ */
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI,{size: "mini"});
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
