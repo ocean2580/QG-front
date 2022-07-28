ef6637824973c7e5c430b1b00f62d9df66aa105f
Ocean <2080917477@qq.com>
2022-07-28 22:11:10 +0800

    vue实现删除（单个及批量）

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -105,13 +105,29 @@
         </div>
 
         <div>
-          <el-button type="primary" @click="handleAdd">新增<i class="el-icon-circle-plus-outline" style="margin-left: 1px"></i></el-button>
-          <el-button type="danger">批量删除<i class="el-icon-remove-outline" style="margin-left: 1px"></i></el-button>
-          <el-button type="primary">导入<i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary" @click="handleAdd">新增<i class="el-icon-circle-plus-outline"
+                                                            style="margin-left: 1px"></i></el-button>
+          <!--  批量删除 -->
+          <el-popconfirm
+              class="ml-5"
+              confirm-button-text='好的'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="确定批量删除这些数据吗？"
+              @confirm="delBatch"
+          >
+            <el-button type="danger" slot="reference">批量删除<i class="el-icon-remove-outline"
+                                                             style="margin-left: 1px"></i></el-button>
+          </el-popconfirm>
+
+          <el-button type="primary" class="ml-5">导入<i class="el-icon-bottom"></i></el-button>
           <el-button type="primary">导出<i class="el-icon-top"></i></el-button>
         </div>
 
-        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg"
+                  @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55"></el-table-column>
           <el-table-column prop="id" label="ID" width="40"></el-table-column>
           <el-table-column prop="username" label="用户名" width="140"></el-table-column>
           <el-table-column prop="nickname" label="昵称" width="120"></el-table-column>
@@ -121,7 +137,19 @@
           <el-table-column label="操作">
             <template slot-scope="scope">
               <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
-              <el-button type="danger">删除 <i class="el-icon-delete"></i></el-button>
+
+              <el-popconfirm
+                  class="ml-5"
+                  confirm-button-text='好的'
+                  cancel-button-text='我再想想'
+                  icon="el-icon-info"
+                  icon-color="red"
+                  title="确定删除吗？"
+                  @confirm="del(scope.row.id)"
+              >
+                <el-button type="danger" slot="reference">删除 <i class="el-icon-delete"></i></el-button>
+              </el-popconfirm>
+
             </template>
           </el-table-column>
         </el-table>
@@ -159,7 +187,7 @@
           </el-form>
           <div slot="footer" class="dialog-footer">
             <el-button @click="dialogFormVisible = false">取 消</el-button>
-            <el-button type="primary"  @click="save">确 定</el-button>
+            <el-button type="primary" @click="save">确 定</el-button>
           </div>
         </el-dialog>
 
@@ -199,6 +227,7 @@ export default {
 
       dialogFormVisible: false,
       form: {},
+      multipleSelection: [],
     }
   },
 
@@ -233,20 +262,42 @@ export default {
           this.$message.success("succeed to save");
           this.dialogFormVisible = false;
           this.load();
-        }
-        else {
+        } else {
           this.$message.error("fail to save");
         }
       })
     },
     handleAdd() {
-      this.dialogFormVisible= true;
+      this.dialogFormVisible = true;
       this.form = {}
     },
     handleEdit(row) {
       this.form = row
       this.dialogFormVisible = true
-
+    },
+    del(id) {
+      request.delete("/user/" + id).then(res => {
+        if (res) {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id);
+      request.post("/user/del/batch", ids).then(res => {
+        if (res) {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
     },
 
     load() {    // 请求封装成方法
