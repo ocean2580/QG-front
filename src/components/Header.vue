b6b646087978af779fb0459ec62724427f43281f
Ocean <2080917477@qq.com>
2022-07-29 17:26:20 +0800

    登录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,21 +3,25 @@
     <div style="flex: 1;font-size: 18px">
       <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
 
-        <el-breadcrumb separator="/" style="display: inline-block; margin-left: 10px">
-          <el-breadcrumb-item :to="'/'">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{currentPathName}}</el-breadcrumb-item>
-        </el-breadcrumb>
+      <el-breadcrumb separator="/" style="display: inline-block; margin-left: 10px">
+        <el-breadcrumb-item :to="'/'">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ currentPathName }}</el-breadcrumb-item>
+      </el-breadcrumb>
     </div>
     <el-dropdown style="width: 70px; cursor: pointer">
       <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
 
       <el-dropdown-menu slot="dropdown" style="width: 100px; text-align: center">
-        <el-dropdown-item>个人信息</el-dropdown-item>
-        <el-dropdown-item>退出</el-dropdown-item>
+        <el-dropdown-item style="font-size: 14px; padding: 5px 0">个人信息</el-dropdown-item>
+        <el-dropdown-item style="font-size: 14px; padding: 5px 0">
+          <router-link to="/login" style="text-decoration: none">
+            退出
+          </router-link>
+        </el-dropdown-item>
       </el-dropdown-menu>
 
     </el-dropdown>
-    </div>
+  </div>
 </template>
 
 <script>
