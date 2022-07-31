108111310fef5040f36179c76138ddb762c69ce1
Ocean <2080917477@qq.com>
2022-07-31 17:03:32 +0800

    首页统计图优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="10">
-        <div id="main" style="width: 500px;height: 400px"></div>
+    <el-row :gutter="10" style="margin-bottom: 40px">
+
+      <el-col :span="6">
+        <el-card style="color: #409eff">
+          <div><i class="el-icon-user-solid"/> 用户总数</div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">
+            100
+          </div>
+        </el-card>
       </el-col>
 
-      <el-col :span="10">
-        <div id="pie" style="width: 800px;height: 600px"></div>
+      <el-col :span="6">
+        <el-card style="color: #f56c6c">
+          <div><i class="el-icon-money"/> 销售总额</div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">
+            $ 100,000,000
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="6">
+        <el-card style="color: #67c23a">
+          <div><i class="el-icon-bank-card"/> 收益总额</div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">$ 90,000,000</div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="6">
+        <el-card style="color: #e6a23c">
+          <div><i class="el-icon-s-shop"/> 门店总数</div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">32</div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="12">
+        <div id="main" style="width: 500px;height: 450px"></div>
+      </el-col>
+
+      <el-col :span="12">
+        <div id="pie" style="width: 500px;height: 400px"></div>
       </el-col>
     </el-row>
   </div>
@@ -29,6 +64,13 @@ export default {
         subtext: "趋势图",
         left: 'center'
       },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
       xAxis: {
         type: 'category',
         data: ["第一季度", "第二季度", "第三季度", "第四季度"]
@@ -38,12 +80,24 @@ export default {
       },
       series: [
         {
+          name: 'Android',
+          data: [],
+          type: 'bar',
+        },
+        {
+          name: 'Android',
           data: [],
           type: 'line',
         },
         {
+          name: 'IOS',
           data: [],
           type: 'bar',
+        },
+        {
+          name: 'IOS',
+          data: [],
+          type: 'line',
         }
       ]
     };
@@ -53,30 +107,68 @@ export default {
     // pie
     var pieOption = {
       title: {
-        text: "各季度注册人数",
-        subtext: "比例图",
+        text: '各季度会员数量统计',
+        subtext: '比例图',
         left: 'center'
       },
-      legend: {
-        top: 'bottom'
+      tooltip: {
+        trigger: 'item'
       },
-      toolbox: {
-        show: true,
-        feature: {
-          mark: {show: true},
-          dataView: { show: true, readOnly: true },
-        }
+      legend: {
+        orient: 'vertical',
+        left: 'left'
       },
       series: [
         {
+          name: 'Android',
           type: 'pie',
-          radius: [50, 250],
-          center: ['50%', '50%'],
-          roseType: 'area',
-          itemStyle: {
-            borderRadius: 8,
+          radius: '50%',
+          center: ['25%', '50%'],
+          label: {            //饼图图形上的文本标签
+            normal: {
+              show: true,
+              position: 'inner', //标签的位置
+              textStyle: {
+                fontWeight: 300,
+                fontSize: 14,    //文字的字体大小
+                color: "#fff"
+              },
+              formatter: '{c} \n {d}%'
+            }
           },
           data: [],  // 填空
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        },
+
+        {
+          name: 'IOS',
+          type: 'pie',
+          radius: '55%',
+          center: ['75%', '50%'],
+          label: {            //饼图图形上的文本标签
+            normal: {
+              show: true,
+              position: 'inner', //标签的位置
+              textStyle: {
+                fontWeight: 300,
+                fontSize: 14,    //文字的字体大小
+                color: "#fff"
+              },
+              formatter: '{c} \n {d}%'
+            }
+          },
+          data: [
+            {name: "第一季度", value: 5},
+            {name: "第二季度", value: 6},
+            {name: "第三季度", value: 7},
+            {name: "第四季度", value: 8},
+          ],
         }
       ]
     };
@@ -88,6 +180,9 @@ export default {
       // option.xAxis.data = res.data.x
       option.series[0].data = res.data
       option.series[1].data = res.data
+
+      option.series[2].data = [5, 6, 7, 8]
+      option.series[3].data = [5, 6, 7, 8]
 
       myChart.setOption(option);
 
