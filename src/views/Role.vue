c15d0a17fb201fd9207a50a4979a266badbab711
Ocean <2080917477@qq.com>
2022-07-31 21:35:55 +0800

    权限功能页面基本实现

---
+++ src/views/Role.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+
+    <div style="margin: 10px 0">
+      <!--    绑定输入框实现模糊搜索      -->
+      <el-input style="width: 200px" suffix-icon="el-icon-search" placeholder="请输入名称" v-model="name"></el-input>
+      <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+      <el-button type="warning" @click="reset">重置</el-button>
+    </div>
+
+    <div>
+      <el-button type="primary" @click="handleAdd">新增<i class="el-icon-circle-plus-outline"
+                                                        style="margin-left: 1px"></i></el-button>
+      <!--  批量删除 -->
+      <el-popconfirm
+          class="ml-5"
+          confirm-button-text='好的'
+          cancel-button-text='我再想想'
+          icon="el-icon-info"
+          icon-color="red"
+          title="确定批量删除这些数据吗？"
+          @confirm="delBatch"
+      >
+        <el-button type="danger" slot="reference">批量删除<i class="el-icon-remove-outline"
+                                                         style="margin-left: 1px"></i></el-button>
+      </el-popconfirm>
+    </div>
+
+    <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"
+              @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column prop="id" label="ID" width="80"></el-table-column>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="description" label="描述"></el-table-column>
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
+
+          <el-popconfirm
+              class="ml-5"
+              confirm-button-text='好的'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="确定删除吗？"
+              @confirm="del(scope.row.id)"
+          >
+            <el-button type="danger" slot="reference">删除 <i class="el-icon-delete"></i></el-button>
+          </el-popconfirm>
+
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div style="padding: 10px 0">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="[2, 5, 10, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
+    </div>
+
+    <!--        dialog -->
+    <el-dialog title="角色信息" :visible.sync="dialogFormVisible" width="30%">
+      <el-form label-width="80px" size="small">
+        <el-form-item label="名称">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Role",
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 2,
+
+      name: "",
+
+      dialogFormVisible: false,
+      form: {},
+      multipleSelection: [],
+
+    }
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    reset() {
+      this.name = "";
+      this.load();
+    },
+
+    // 增删查改
+    save() {
+      this.request.post("/role", this.form).then(res => {
+        if (res.code === '200') {
+          this.$message.success("succeed to save");
+          this.dialogFormVisible = false;
+          this.load();
+        } else {
+          this.$message.error("fail to save");
+        }
+      })
+    },
+    handleAdd() {
+      this.dialogFormVisible = true;
+      this.form = {}
+    },
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row));
+      this.dialogFormVisible = true;
+    },
+    del(id) {
+      this.request.delete("/role/" + id).then(res => {
+        if (res.code === '200') {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id);
+      this.request.post("/role/del/batch", ids).then(res => {
+        if (res.code === '200') {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
+    },
+
+    load() {    // 请求封装成方法
+      this.request.get("/role/page", {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          name: this.name,
+
+        }
+      }).then(res => {
+        console.log(res);
+        this.tableData = res.data.records
+        this.total = res.data.total;
+      })
+    },
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize;
+      this.load();
+    },
+    handleCurrentChange(pageNum) {
+      this.pageNum = pageNum;
+      this.load();
+    },
+
+  }
+}
+</script>
+
+<style>
+.headerBg {
+  background: #eee !important;
+}
+</style>
