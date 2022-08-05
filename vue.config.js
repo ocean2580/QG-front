3b7060484c38e4b75c133fe6dcb916a97238c30e
Ocean <2080917477@qq.com>
2022-08-05 08:33:05 +0800

    摸鱼狂魔的自我修养

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    client: {
+      webSocketURL: 'ws//192.168.43.66:8080/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },
+
   transpileDependencies: true
 })
