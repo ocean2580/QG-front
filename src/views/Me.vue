98957563377fb7806e8808029829b12bb1c51f65
Ocean <2080917477@qq.com>
2022-08-05 11:01:09 +0800

    添加support页面

---
+++ src/views/Me.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+    <div><h1>Maybe you can support him a cup of coffee~</h1></div> <br/>
+
+    <div>
+      <img src="../assets/deer.jpg" alt="" width="337px" height="422px">
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: "Me"
+}
+</script>
+
+<style scoped>
+
+</style>
