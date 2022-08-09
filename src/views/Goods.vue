fbab5b9f875cc9a326bc64ab3c88818c17aa4317
Ocean <2080917477@qq.com>
2022-08-09 13:39:18 +0800

    goods完成 token亟待完善

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -1,13 +1,216 @@
 <template>
-  <div>goods</div>
+  <div>
+
+    <div style="margin: 10px 0">
+      <!--    绑定输入框实现模糊搜索      -->
+      <el-input style="width: 200px" suffix-icon="el-icon-search" placeholder="请输入名称" v-model="goodsName" class="ml-5"></el-input>
+      <el-input style="width: 200px" suffix-icon="el-icon-search" placeholder="请输入丢失地点" v-model="lostPosition" class="ml-5"></el-input>
+      <el-input style="width: 200px" suffix-icon="el-icon-search" placeholder="请输入领取地点" v-model="claimPosition" class="ml-5"></el-input>
+
+      <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+      <el-button type="warning" @click="reset">重置</el-button>
+    </div>
+
+    <div>
+      <el-button type="primary" @click="handleAdd">新增<i class="el-icon-circle-plus-outline"
+                                                        style="margin-left: 1px"></i></el-button>
+      <!--  批量删除 -->
+      <el-popconfirm
+          class="ml-5"
+          confirm-button-text='好的'
+          cancel-button-text='我再想想'
+          icon="el-icon-info"
+          icon-color="red"
+          title="确定批量删除这些数据吗？"
+          @confirm="delBatch"
+      >
+        <el-button type="danger" slot="reference">批量删除<i class="el-icon-remove-outline"
+                                                         style="margin-left: 1px"></i></el-button>
+      </el-popconfirm>
+
+
+    </div>
+
+    <!-- table  -->
+    <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"
+              @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+
+      <!--  与 entity类 字段名一致    -->
+      <el-table-column prop="id" label="ID" width="40"></el-table-column>
+      <el-table-column prop="goodsName" label="名称" width="140"></el-table-column>
+      <el-table-column prop="lostPosition" label="丢失地点"></el-table-column>
+      <el-table-column prop="claimPosition" label="领取地点" width="120"></el-table-column>
+      <el-table-column prop="description" label="描述"></el-table-column>
+      <el-table-column prop="lostDate" label="丢失时间"></el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
+
+          <el-popconfirm
+              class="ml-5"
+              confirm-button-text='好的'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="确定删除吗？"
+              @confirm="del(scope.row.id)"
+          >
+            <el-button type="danger" slot="reference">删除 <i class="el-icon-delete"></i></el-button>
+          </el-popconfirm>
+
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div style="padding: 10px 0">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="[2, 5, 10, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
+    </div>
+
+    <!--        dialog ~ form       -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible" width="30%">
+      <el-form label-width="80px" size="small">
+        <el-form-item label="名称">
+          <el-input v-model="form.goodsName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="丢失地点">
+          <el-input v-model="form.lostPosition" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="领取地点">
+          <el-input v-model="form.claimPosition" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description" autocomplete="off"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Goods"
+  name: "Goods",
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 2,
+
+      goodsName: "",
+      lostPosition: "",
+      claimPosition: "",
+
+
+      dialogFormVisible: false,
+      form: {},
+      multipleSelection: [],
+      roles: []
+
+    }
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    reset() {
+      this.goodsName = "";
+      this.lostPosition = "";
+      this.claimPosition = "";
+      this.load();
+    },
+
+    // 增删查改
+    save() {
+      this.request.post("/goods", this.form).then(res => {
+        if (res.data) {
+          this.$message.success("succeed to save");
+          this.dialogFormVisible = false;
+          this.load();
+        } else {
+          this.$message.error("fail to save");
+        }
+      })
+    },
+    handleAdd() {
+      this.dialogFormVisible = true;
+      this.form = {}
+    },
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row));
+      this.dialogFormVisible = true;
+    },
+    del(id) {
+      this.request.delete("/goods/" + id).then(res => {
+        if (res.data) {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id);
+      this.request.post("/goods/del/batch", ids).then(res => {
+        if (res.data) {
+          this.$message.success("succeed to delete");
+          this.load();
+        } else {
+          this.$message.error("fail to delete");
+        }
+      })
+    },
+
+    load() {    // 请求封装成方法
+      this.request.get("/goods/page", {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          goodsName: this.goodsName,
+          lostPosition: this.lostPosition,
+          claimPosition: this.claimPosition
+        }
+      }).then(res => {
+        console.log(res);
+        this.tableData = res.data.records
+        this.total = res.data.total;
+      })
+
+    },
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize;
+      this.load();
+    },
+    handleCurrentChange(pageNum) {
+      this.pageNum = pageNum;
+      this.load();
+    },
+
+
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+.headerBg {
+  background: #eee !important;
+}
 </style>
--- src/views/Me.vue
+++ src/views/Me.vue
@@ -3,8 +3,16 @@
   <div>
     <div><h1>Maybe you can support him a cup of coffee~</h1></div> <br/>
 
-    <div>
-      <img src="../assets/deer.jpg" alt="" width="337px" height="422px">
+    <div style="margin-top: 20px">
+      <img src="../assets/wechat.jpg" alt="" width="200px" height="200px">
+    </div>
+
+    <div style="margin-top: 30px">
+      <a href="https://github.com/ocean2580" target ="_blank">github主页</a><br/>
+    </div>
+
+    <div style="margin-top: 30px">
+      <a href="https://www.bilibili.com/video/BV1U44y1W77D?share_source=copy_web&vd_source=6f7c3eb307225bf097074bea8a4701de" target ="_blank">项目学习地址</a>
     </div>
   </div>
 
